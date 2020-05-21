c1d254d21adc4eb2119f6ae65891011d94901c92
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-05-21 04:15:37 -0400

    updated travis file

--- knexfile.js
+++ knexfile.js
@@ -19,6 +19,7 @@ module.exports = {
     client: "pg",
     connection: {
       host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
       database: process.env.DATABASE_NAME,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
