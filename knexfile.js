adff08a99b180ceeb299b2dc08cd07b45bd5520c
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-05-21 15:07:22 -0400

    updated prod db config

--- knexfile.js
+++ knexfile.js
@@ -29,12 +29,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: {
-      host: process.env.DATABASE_HOST,
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
