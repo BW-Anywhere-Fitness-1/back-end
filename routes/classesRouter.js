1170b8b468a1ee15486eb14da2f6bbc849ce8387
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-05-22 19:14:28 -0400

    updated doc

--- routes/classesRouter.js
+++ routes/classesRouter.js
@@ -3,34 +3,42 @@ const ClassesController = require("./../controllers/ClassesController");
 const authenticate = require("./../controllers/auth/auth-middleware");
 const authAsInstructor = require("./../controllers/auth/auth-instructor");
 
-router.get(
-  "/classes",
-  authenticate,
-  ClassesController.index.bind(ClassesController)
-);
+// required to be logged in as instructor
+// create class
 router.post(
   "/classes",
   authenticate,
   authAsInstructor,
   ClassesController.create.bind(ClassesController)
 );
-router.get(
-  "/classes/:id",
-  authenticate,
-  ClassesController.show.bind(ClassesController)
-);
+// update class
 router.put(
   "/classes/:id",
   authenticate,
   authAsInstructor,
   ClassesController.update.bind(ClassesController)
 );
+// delete class
 router.delete(
   "/classes/:id",
   authenticate,
   authAsInstructor,
   ClassesController.remove.bind(ClassesController)
 );
+// not required to be logged in as instructor routes
+// retrieve class list
+router.get(
+  "/classes",
+  authenticate,
+  ClassesController.index.bind(ClassesController)
+);
+// get one class by id
+router.get(
+  "/classes/:id",
+  authenticate,
+  ClassesController.show.bind(ClassesController)
+);
+// search by name, start_time, duration, schedule, type, level
 router.get(
   "/search/classes",
   authenticate,
